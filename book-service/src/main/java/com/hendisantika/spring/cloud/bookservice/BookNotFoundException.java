c91721bb1c5b27e8ef5d34ca94b9782a8a37fd7f
Hendi Santika <hendisantika@gmail.com>
2017-10-31 07:36:25 +0700

    #8 - Added Book Service - book-service

---
+++ book-service/src/main/java/com/hendisantika/spring/cloud/bookservice/BookNotFoundException.java
@@ -0,0 +1,16 @@
+package com.hendisantika.spring.cloud.bookservice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-bootstrap
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/10/17
+ * Time: 07.35
+ * To change this template use File | Settings | File Templates.
+ */
+public class BookNotFoundException extends X {
+    public BookNotFoundException(String s) {
+    }
+}
---
+++ book-service/src/main/java/com/hendisantika/spring/cloud/bookservice/BookService.java
@@ -0,0 +1,74 @@
+package com.hendisantika.spring.cloud.bookservice;
+
+import com.google.common.base.Preconditions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-bootstrap
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/10/17
+ * Time: 07.35
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Service
+@Transactional(readOnly = true)
+public class BookService {
+    @Autowired
+    private BookRepository bookRepository;
+
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book findBookById(Long bookId) {
+        return Optional.ofNullable(bookRepository.findOne(bookId))
+                .orElseThrow(() -> new BookNotFoundException("Book not found. ID: " + bookId));
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Book createBook(Book book) {
+        final Book newBook = new Book();
+        newBook.setTitle(book.getTitle());
+        newBook.setAuthor(book.getAuthor());
+        return bookRepository.save(newBook);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public void deleteBook(Long bookId) {
+        bookRepository.delete(bookId);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Book updateBook(Map<String, String> updates, Long bookId) {
+        final Book book = findBookById(bookId);
+        updates.keySet()
+                .forEach(key -> {
+                    switch (key) {
+                        case "author":
+                            book.setAuthor(updates.get(key));
+                            break;
+                        case "title":
+                            book.setTitle(updates.get(key));
+                    }
+                });
+        return bookRepository.save(book);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Book updateBook(Book book, Long bookId) {
+        Preconditions.checkNotNull(book);
+        Preconditions.checkState(book.getId() == bookId);
+        Preconditions.checkNotNull(bookRepository.findOne(bookId));
+        return bookRepository.save(book);
+    }
+}
