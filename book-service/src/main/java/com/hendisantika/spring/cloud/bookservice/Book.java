3e9244907573486d6d80f2b94ed9631424e4efdd
Hendi Santika <hendisantika@gmail.com>
2017-10-31 07:31:13 +0700

    #4 - Modified parent pom.xml

---
+++ book-service/src/main/java/com/hendisantika/spring/cloud/bookservice/Book.java
@@ -0,0 +1,14 @@
+package com.hendisantika.spring.cloud.bookservice;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-cloud-bootstrap
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 31/10/17
+ * Time: 06.03
+ * To change this template use File | Settings | File Templates.
+ */
+public class Book {
+}
