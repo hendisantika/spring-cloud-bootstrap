efbb49d41e985bd37cced8715cd3f6c38072a2ca
Hendi Santika <hendisantika@gmail.com>
2017-10-31 07:34:00 +0700

    #6 - Added Book Model - book-service

--- book-service/src/main/java/com/hendisantika/spring/cloud/bookservice/Book.java
+++ book-service/src/main/java/com/hendisantika/spring/cloud/bookservice/Book.java
@@ -1,5 +1,10 @@
 package com.hendisantika.spring.cloud.bookservice;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-cloud-bootstrap
@@ -10,5 +15,46 @@ package com.hendisantika.spring.cloud.bookservice;
  * Time: 06.03
  * To change this template use File | Settings | File Templates.
  */
+
+@Entity
+
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String author;
+    private String title;
+
+    public Book() {
+    }
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
